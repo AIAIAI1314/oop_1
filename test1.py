483027d88f41b1ccd39357f4c2b1bd3a7e259a1c
AIAIAI1314 <axhw1314@163.com>
2019-01-31 17:26:06 +0800

    test1

--- test1.py
+++ test1.py
@@ -13,18 +13,25 @@ def doc():
     help(doc)
     doc.__doc__
 
+
 class Hero():
     pass
 wizard = Hero()
+
 
 class DefendBuilding():
     level = 1
     ATK = 100
     HP = 500
     # 属性
+
     def action(self):
         print("防御")
         return None
     # 动作或功能
 acher_tower = DefendBuilding()
 acher_tower.action()
+print("弓箭塔的等级是:", acher_tower.level)
+print("弓箭塔的攻击力是:", acher_tower.ATK)
+
+print(DefendBuilding.__dict__)
